438fedc6a164981f02037930038da31db67e3271
david-erb <david-erb@users.noreply.github.com>
2023-05-29 17:34:44 +0000

    deploy: 9b42eb0dd043b3c08256ea1a48c51e73e995ab33

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.2.dev2+g6cd9698',
+    VERSION: '1.1.2.dev3+g9b42eb0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
